c4600f6085660d2441122a526265c08008a4ce2d
Victor Ng <vng@brandeis.edu>
2021-12-29 00:44:29 -0600

    pck stuf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement("h1", {}, "Adopt me?"),
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
