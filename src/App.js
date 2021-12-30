4631cf05aea23f8aac6d924c02563790c7add7f4
Victor Ng <vng@brandeis.edu>
2021-12-30 17:10:31 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Pet from "./Pet";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me?"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Squwak",
-      animal: "Parrot",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Meow", animal: "Cat", breed: "Persian" }),
-  ]);
+  return (
+    <div>
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt me?</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
